36c1920ba067dc53ee615f60a928056a69ad7a03
Waled Karim <waledkarim445@gmail.com>
2025-05-11 13:52:50 +0600

    Chapter 3 completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
 
 export default function RootLayout({
@@ -6,9 +7,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  console.log(inter);
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
