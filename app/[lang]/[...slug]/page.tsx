94a473c0de0101f0af67e805f323421db11bb082
Waled Karim <waledkarim445@gmail.com>
2025-05-12 20:50:56 +0600

    Learnt dynamic metadata generation

--- app/[lang]/[...slug]/page.tsx
+++ app/[lang]/[...slug]/page.tsx
@@ -1,6 +1,18 @@
+import { Metadata } from "next";
+
+type PageParams = {
+    params: Promise<{lang: string, slug: string[]}>
+}
+
+export const generateMetadata = async ({ params }: PageParams): Promise<Metadata> => {
+    const { lang } = await params;
+    return {
+        title: `Language is ${lang}`
+    }
+}
 
 
-export default async function({ params }: { params: Promise<{lang: string, slug: string[]}> }){
+export default async function({ params }: PageParams){
     const{ lang, slug } = await params;
     return(
         <>
